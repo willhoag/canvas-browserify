888c6dda4a4554cde03092165eee1bfe0de22b03
willhoag <will@willhoag.com>
2015-08-07 22:10:00 -0700

    change instantating class into a function call

--- browser.js
+++ browser.js
@@ -1,15 +1,11 @@
-
-var Canvas = module.exports = function Canvas (w, h) {
+var canvas = module.exports = function canvas (w, h) {
   var canvas = document.createElement('canvas')
   canvas.width = w || 300
   canvas.height = h || 150
   return canvas
 }
 
-Canvas.Image = function () {
+canvas.Image = function () {
   var img = document.createElement('img')
   return img
 }
-
-
-
